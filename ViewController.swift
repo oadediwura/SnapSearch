cd7ef97f907f3f02976ced96dac096e6db6a0643
Taiwo Adelabu <taiwoadelabu2015@gmail.com>
2016-08-07 16:29:26 -0400

    Clean up the project

--- ViewController.swift
+++ ViewController.swift
@@ -5,7 +5,6 @@
 //  Copyright ©2016 Taiwo Adelabu, K.Swain & D.Nwankwo. All rights reserved.
 
 import UIKit
-import Alamofire
 import CloudSight
 import LLSimpleCamera
 import SafariServices
@@ -13,10 +12,13 @@ import SafariServices
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, CloudSightImageRequestDelegate, CloudSightQueryDelegate {
     @IBOutlet weak var imageSaved: UIImageView!
     @IBOutlet weak var restartLabel: UIButton!
-
+    
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet var imageView: UIImageView!
     
+    
+    var imagePicker: UIImagePickerController!
     var myImage: UIImage?
     var switchButton = UIButton();
     var flashButton = UIButton()
@@ -29,6 +31,15 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     @IBOutlet weak var scrollContainerView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        additionalSetup()
+        addSwitchButton()
+        addFlashButton()
+        isFlashAvailable()
+        titleLabelGestureRecogonizer()
+        
+    }
+    
+    func additionalSetup() {
         // Do any additional setup after loading the view, typically from a nib.
         
         let screenRect = UIScreen.mainScreen().bounds
@@ -42,7 +53,9 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         self.titleLabel.hidden = true
         self.restartLabel.hidden = true
         self.saveButton.hidden = true
-        
+    }
+    
+    func addSwitchButton() {
         self.switchButton = UIButton(type: .System)
         self.switchButton.frame = CGRectMake(self.view.frame.width - 60, 20, 29.0 + 20.0, 22.0 + 20.0)
         self.switchButton.tintColor = UIColor.whiteColor()
@@ -50,40 +63,45 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         self.switchButton.imageEdgeInsets = UIEdgeInsetsMake(10, 10.0, 10.0, 10)
         self.switchButton.addTarget(self, action: #selector(ViewController.switchButtonPressed(_:)), forControlEvents: .TouchUpInside)
         self.camera.view.addSubview(self.switchButton)
-        
+    }
+    
+    func addFlashButton() {
         self.flashButton = UIButton(type: .System)
         self.flashButton.frame = CGRectMake(self.view.frame.width - 120, 20, 29.0 + 20.0, 22.0 + 20.0)
         self.flashButton.tintColor = UIColor.whiteColor()
         self.flashButton.setImage(UIImage(named: "camera-flash.png"), forState: .Normal)
         self.flashButton.imageEdgeInsets = UIEdgeInsetsMake(10.0, 10.0, 10.0, 10.0)
-        self.flashButton.addTarget(self, action: "flashButtonPressed:", forControlEvents: .TouchUpInside)
+        self.flashButton.addTarget(self, action: #selector(ViewController.flashButtonPressed(_:)), forControlEvents: .TouchUpInside)
         //self.flashButton.hidden = true;
         self.view!.addSubview(self.flashButton)
+    }
     
-         self.camera.onDeviceChange = {(camera, device) -> Void in
-         if camera.isFlashAvailable() {
-         self.flashButton.hidden = false
-         if camera.flash == LLCameraFlashOff {
-         self.flashButton.selected = false
-         }
-         else {
-         self.flashButton.selected = true
-         }
-         }
-         else {
-         self.flashButton.hidden = true
-         }
-         }
-        
-        // title label
-        
-        self.titleLabel.userInteractionEnabled = true
-        
-        let labelGesture = UITapGestureRecognizer(target: self, action: Selector("showSearch"))
-        titleLabel.addGestureRecognizer(labelGesture)
+    func isFlashAvailable() {
+        self.camera.onDeviceChange = {(camera, device) -> Void in
+            if camera.isFlashAvailable() {
+                self.flashButton.hidden = false
+                if camera.flash == LLCameraFlashOff {
+                    self.flashButton.selected = false
+                }
+                else {
+                    self.flashButton.selected = true
+                }
+            }
+            else {
+                self.flashButton.hidden = true
+            }
+        }
         
     }
     
+    
+    func titleLabelGestureRecogonizer() {
+        // title label
+        self.titleLabel.userInteractionEnabled = true
+        
+        let labelGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.showSearch))
+        titleLabel.addGestureRecognizer(labelGesture)
+    }
     
     
     override func viewWillAppear(animated: Bool) {
@@ -98,7 +116,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         
         self.camera.capture { (camera, image, metadata, error) in
             self.myImage = image
-            self.AlamoFireRequest()
+            self.cloudServiceCall()
         }
         let btn = sender as? UIButton
         snapButton = btn!
@@ -114,7 +132,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         }
         
         self.camera.togglePosition()
-    
+        
     }
     
     func flashButtonPressed(button: UIButton) {
@@ -133,15 +151,13 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
             }
         }
     }
-
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    var imagePicker: UIImagePickerController!
-    @IBOutlet var imageView: UIImageView!
+
     @IBAction func takePhoto(sender: UIButton) {
         imagePicker = UIImagePickerController()
         imagePicker.delegate = self
@@ -153,50 +169,26 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         
     }
     
+    // TBD
+    
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         imagePicker.dismissViewControllerAnimated(true, completion: nil)
         imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
         self.myImage = imageView.image
-        self.AlamoFireRequest()
+        self.cloudServiceCall()
         
     }
     
-    // With Alamofire
-    //    func fetchAllRooms() {
-    //
-    //        Alamofire.upload(
-    //            .POST,
-    //            "https://api.cloudsightapi.com//image_requests",
-    //            headers: ["Authorization" : "CloudSight [n2o53offxe1q5WJC4Dzm0Q]"],
-    //            multipartFormData: { multipartFormData in
-    //                multipartFormData.appendBodyPart(data: self.myImage!, name: "imagefile",
-    //                    fileName: "image.jpg", mimeType: "image/jpeg")
-    //            },
-    //            encodingCompletion: { encodingResult in
-    //
-    //            }
-    //    }
     
-    
-    func AlamoFireRequest () {
+    func cloudServiceCall() {
         
         
-        CloudSightConnection.sharedInstance().consumerKey = "n2o53offxe1q5WJC4Dzm0Q"
-        CloudSightConnection.sharedInstance().consumerSecret = "g_eBushHVJmxtuKPBPSCaw"
+         CloudSightConnection.sharedInstance().consumerKey = "n2o53offxe1q5WJC4Dzm0Q"
+         CloudSightConnection.sharedInstance().consumerSecret = "g_eBushHVJmxtuKPBPSCaw"
+       // CloudSightConnection.sharedInstance().consumerKey = "DkWlFtnLP7wFkytv0w-Ubg"
+       // CloudSightConnection.sharedInstance().consumerSecret = "Hk1QtCNn0rt05OqUCnOEJQ"
         
         self.searchWithImage(self.myImage!)
-        
-        //            Alamofire.request(.POST, "http://cloudsightapi.com/image_requests/", headers: ["Authorization":"CloudSight n2o53offxe1q5WJC4Dzm0Q",] ,parameters: ["image_request[image]": self.myImage!, "image_request[locale]":"en-US", "Secret":"g_eBushHVJmxtuKPBPSCaw", "API Key":"n2o53offxe1q5WJC4Dzm0Q"])
-        //                .responseJSON { response in
-        //                    print(response.request)  // original URL request
-        //                    print(response.response) // URL response
-        //                    print(response.data)     // server data
-        //                    print(response.result)   // result of response serialization
-        //
-        //                    if let JSON = response.result.value {
-        //                        print("JSON: \(JSON)")
-        //                    }
-        //            }
     }
     
     var cloudQuery : CloudSightQuery?
@@ -215,7 +207,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         // Start the query process
         query.start()
         self.cloudQuery = query
-
+        
     }
     
     // MARK: - CloudSightImageRequestDelegate
@@ -239,23 +231,15 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         dispatch_async(dispatch_get_main_queue()) {
             if query.title != nil {
                 print(query.title)
-        
+                
                 var str: String = query.title
                 
                 
                 str.replaceRange(str.startIndex...str.startIndex, with: String(str[str.startIndex]).capitalizedString)
                 self.titleLabel.text = str
             }
-            self.activityIndicator.hidden = true
-            self.titleLabel.hidden = false
-            self.camera.stop()
-            self.activityIndicator.stopAnimating()
-            self.restartLabel.hidden = false
-            self.saveButton.hidden = false
-            self.switchButton.hidden = true
-            self.flashButton.hidden = true
-            self.titleLabel.hidden = false
-
+  
+            self.showReset()
         }
     }
     
@@ -264,7 +248,21 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
             print(error)
             self.activityIndicator.hidden = true
             self.activityIndicator.stopAnimating()
+            
+            self.showReset()
         }
+    }
+    
+    func showReset() {
+        self.activityIndicator.hidden = true
+        self.titleLabel.hidden = false
+        self.camera.stop()
+        self.activityIndicator.stopAnimating()
+        self.restartLabel.hidden = false
+        self.saveButton.hidden = false
+        self.switchButton.hidden = true
+        self.flashButton.hidden = true
+        self.titleLabel.hidden = false
     }
     
     @IBAction func restartCamera(sender: AnyObject) {
@@ -278,51 +276,21 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         isSafari = false
     }
     
-    
     @IBAction func save(sender: AnyObject) {
         
         
-       if let myImage = myImage {
+        if let myImage = myImage {
             UIImageWriteToSavedPhotosAlbum(myImage, self, nil, nil)
             self.imageSaved.alpha = 1.0
-        UIView.animateWithDuration(4.0, delay: 0.1, options: .CurveEaseOut, animations: {
-            self.imageSaved.alpha = 0.0
-
-            }, completion: nil)
-
-
-       }
+            UIView.animateWithDuration(4.0, delay: 0.1, options: .CurveEaseOut, animations: {
+                self.imageSaved.alpha = 0.0
+                
+                }, completion: nil)
+            
+            
+        }
     }
     
-    //
-    //        Alamofire.request(
-    //            .GET,
-    //            "http://localhost:5984/rooms/_all_docs",
-    //            parameters: ["include_docs": "true"],
-    //            encoding: .URL)
-    //            .validate()
-    //            .responseJSON { (response) -> Void in
-    //                guard response.result.isSuccess else {
-    //                    print("Error while fetching remote rooms: \(response.result.error)")
-    //                    completion(nil)
-    //                    return
-    //                }
-    //
-    //                guard let value = response.result.value as? [String: AnyObject],
-    //                    rows = value["rows"] as? [[String: AnyObject]] else {
-    //                        print("Malformed data received from fetchAllRooms service")
-    //                        completion(nil)
-    //                        return
-    //                }
-    //
-    //                var rooms = [RemoteRoom]()
-    //                for roomDict in rows {
-    //                    rooms.append(RemoteRoom(jsonData: roomDict))
-    //                }
-    //
-    //                completion(rooms)
-    //        }
-    //    }
     
     func showSearch() {
         print("Label Touched")
